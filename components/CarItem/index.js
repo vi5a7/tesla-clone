ef14d504ad8b09784713b2b9c152eb45fe4542e4
vi5a7 <udamvisal18@kit.edu.kh>
2021-05-03 14:18:36 +0700

    added Button Component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, ImageBackground, StatusBar } from 'react-native'
 import styles from './styles'
+import StyledButton from '../StyledButton/index';
 
 const CarItem = () => {
     return (
@@ -13,7 +14,20 @@ const CarItem = () => {
                 <Text style={styles.title}>Model S</Text>
                 <Text style={styles.subtitle}>Starting at $69, 428</Text>
             </View>
-            <StatusBar style='auto'/>
+            <StyledButton 
+                type='primary'
+                content={'Custom Order'} 
+                onPress={() => {
+                    console.warn('Custom Order was pressed')
+                }}
+            />
+            <StyledButton 
+                type='secondary'
+                content={'Existing Inventory'} 
+                onPress={() => {
+                    console.warn('Existing Inventory was pressed')
+                }}
+            />
         </View>
     );
 }
